12e8eace37d4be5f284bafa7bbaf31d5a4e76447
rpss2 <rpss@cin.ufpe.br>
2018-07-11 02:17:00 -0300

    datas manipulation

--- web/script.js
+++ web/script.js
@@ -1,6 +1,14 @@
 let src = "https://code.angularjs.org/1.3.8/angular.min.js"
 let url = "http://localhost:3000/"
 
+let emotionMatch = {
+        "sad" : "sadness",
+        "angry" : "anger",
+        "happy" : "joy",
+        "fear" : "fear",
+        "disgust" : "disgust",
+    };
+
 var mainMod = angular.module('MainApp', []);
             mainMod.controller('MainCtrl', function ($scope) {
                 $scope.text = '';
@@ -16,10 +24,31 @@ function sendToBack() {
 
     axios.post(`${url}get-analyse-text`, data)
     .then(res => {
-        console.log(res.data);
+        let result = manipulationOfDatas(res.data);
+        console.log(result);
     })
     .catch(err => {
         console.log(err);
     });
     
+}
+
+function manipulationOfDatas(data) {
+    let text = data.text;
+    let audio = data.audio;
+
+    let dataSets = {
+        audio: [],
+        text: [],
+    };
+
+    for(let i = 0; i < audio.length; i++) {
+        let emotion = audio[i].emotion;
+        if(match = emotionMatch[emotion]) {
+            dataSets.text.push(text[match]);
+            dataSets.audio.push(audio[i].score);
+        }
+    }
+
+    return dataSets;
 }
