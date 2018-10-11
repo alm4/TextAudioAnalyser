921e7b22cc68c5b72728105870957056e46b6cb7
Arthur Magno <alma4@cin.ufpe.br>
2018-10-11 01:39:48 -0300

    Alterações

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const API = require('./API');
 app.use(express.static(__dirname + '/web'));
 app.use(require('body-parser').json());
 
+var cors = require('cors');
+
+// use it before all route definitions
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -20,15 +25,15 @@ app.get('/', function(req, res) {
 app.post('/get-analyse-text', function(req, res) {
     API.getAnalyseText(req.body.url, function(data) {
         console.log(data.emotion.document.emotion);
-        axios.post(`http://localhost:3000/get-analyse-audio`, {url: req.body.url})
-        .then(response => {
-            let infos = {
-                text: data.emotion.document.emotion,
-                audio: response.data
-            }
-            res.send(infos);
-        })
-        .catch(err => {});
+        axios.post(`http://localhost:3000/get-analyse-audio`, { url: req.body.url })
+            .then(response => {
+                let infos = {
+                    text: data.emotion.document.emotion,
+                    audio: response.data
+                }
+                res.send(infos);
+            })
+            .catch(err => {});
     });
 });
 
