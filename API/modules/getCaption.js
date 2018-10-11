77808d95768faf421f03fa9d9162a309a86930f3
Arthur Magno <alma4@cin.ufpe.br>
2018-10-11 01:13:55 -0300

    Alterações

--- API/modules/getCaption.js
+++ API/modules/getCaption.js
@@ -1,35 +1,36 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true });
+const nightmare = Nightmare({ show: false });
 const cheerio = require('cheerio');
 const htmlEntities = require('html-entities').AllHtmlEntities;
 
 // Imports the Google Cloud client library
-const {Translate} = require('@google-cloud/translate');
+const { Translate } = require('@google-cloud/translate');
 
 // Your Google Cloud Platform project ID
 const projectId = 'f4712f42ef2d4ed7cf0a6508dfda3dbe5cca603a';
 
 // Instantiates a client
 const translate = new Translate({
-  projectId: projectId,
-  keyFilename: './Translate-Analyzer-f4712f42ef2d.json'
+    projectId: projectId,
+    keyFilename: './Translate-Analyzer-f4712f42ef2d.json'
 });
 
 //var googleTranslate = require('google-translate')('AIzaSyCwiDzro_Ac_fro7ml8m8pNGNTUiaPYJdk');
 
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-  'username': '7aef01db-a03c-4b75-9ccf-bc88dd993152',
-  'password': '2WgBo3WgQgK6',
-  'version': '2018-03-16'
+    'username': '7aef01db-a03c-4b75-9ccf-bc88dd993152',
+    'password': '2WgBo3WgQgK6',
+    'version': '2018-03-16'
 });
 
 function getEmotion(url, callback) {
 
+    console.log("I am here");
     nightmare
-        .goto(url)    
-        .wait('.dropdown-trigger.style-scope.ytd-menu-renderer > button.style-scope.yt-icon-button')//.wait('[aria-label="Mais ações"]')
-        .click('.dropdown-trigger.style-scope.ytd-menu-renderer > button.style-scope.yt-icon-button')//.click('[aria-label="Mais ações"]')
+        .goto(url)
+        .wait('.dropdown-trigger.style-scope.ytd-menu-renderer > button.style-scope.yt-icon-button') //.wait('[aria-label="Mais ações"]')
+        .click('.dropdown-trigger.style-scope.ytd-menu-renderer > button.style-scope.yt-icon-button') //.click('[aria-label="Mais ações"]')
         .wait('ytd-menu-service-item-renderer')
         .click('ytd-menu-service-item-renderer')
         .wait('#transcript > ytd-transcript-renderer #body ytd-transcript-body-renderer')
@@ -38,14 +39,14 @@ function getEmotion(url, callback) {
             return document.querySelector('#transcript > ytd-transcript-renderer #body ytd-transcript-body-renderer')
                 .innerHTML;
         })
-        .end()    
+        .end()
         .then(content => {
             const $ = cheerio.load(content);
 
             const elems = $('.cue-group-start-offset');
             const transcript = {};
-            elems.each(function (index, element) {
-                transcript[$(element).html().trim()] = htmlEntities.decode($(element).next().children().html().trim());            
+            elems.each(function(index, element) {
+                transcript[$(element).html().trim()] = htmlEntities.decode($(element).next().children().html().trim());
             });
 
             console.log(transcript);
@@ -56,9 +57,9 @@ function getEmotion(url, callback) {
             let result = ``
 
             for (let prop in transcript) {
-                if(transcript.hasOwnProperty(prop)) {
-                    if(transcript[prop] !== "")
-                        if(result === ``)
+                if (transcript.hasOwnProperty(prop)) {
+                    if (transcript[prop] !== "")
+                        if (result === ``)
                             result = `${transcript[prop]}`
                         else
                             result = `${result}, ${transcript[prop]}`
@@ -81,55 +82,54 @@ function getEmotion(url, callback) {
             // multiple texts.
 
             translate
-              .translate(text, target)
-              .then(results => {
-                const translation = results[0];
+                .translate(text, target)
+                .then(results => {
+                    const translation = results[0];
 
-                //console.log("");
-                //console.log(`Text: ${text}`);
-                //console.log("");
-                //console.log(`Translation: ${translation}`);
-                //console.log("");
+                    //console.log("");
+                    //console.log(`Text: ${text}`);
+                    //console.log("");
+                    //console.log(`Translation: ${translation}`);
+                    //console.log("");
 
-                //return translation;
+                    //return translation;
 
-                console.log("-----------------");
+                    console.log("-----------------");
 
-                console.log("Translated text input");
-                console.log("-----------------");
-                console.log(translation);
-                console.log("-----------------");
+                    console.log("Translated text input");
+                    console.log("-----------------");
+                    console.log(translation);
+                    console.log("-----------------");
 
-                console.log("WATSOM SAID  : ");
+                    console.log("WATSOM SAID  : ");
 
-                var parameters = {
-                    'text': translation,
-                    'features': {
-                    'emotion': {},
-                    'sentiment': {},
-                    'categories': {
-                        'limit': 2,
-                    },
-                   'concepts': {
-                        'limit': 2,
+                    var parameters = {
+                        'text': translation,
+                        'features': {
+                            'emotion': {},
+                            'sentiment': {},
+                            'categories': {
+                                'limit': 2,
+                            },
+                            'concepts': {
+                                'limit': 2,
+                            }
+                        }
                     }
-                   }
-                }
-            
-                natural_language_understanding.analyze(parameters, function(err, response) {
-                   if (err) {
-                      console.log('error:', err);
-                   }
-                   else {
-                      //console.log(JSON.stringify(response, null, 2));
-                      callback(response);
-                  }
-                });
 
-              })
-              .catch(err => {
-                 console.error('ERROR:', err);
-              });
+                    natural_language_understanding.analyze(parameters, function(err, response) {
+                        if (err) {
+                            console.log('error:', err);
+                        } else {
+                            //console.log(JSON.stringify(response, null, 2));
+                            callback(response);
+                        }
+                    });
+
+                })
+                .catch(err => {
+                    console.error('ERROR:', err);
+                });
 
             //googleTranslate.translate(result, 'en', function(err, translation) {
             //  console.log(translation.translatedText);
