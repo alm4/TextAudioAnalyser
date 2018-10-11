cdf8e6e7a3a538e8de1ef8c14472e8ee60835746
Arthur Magno <alma4@cin.ufpe.br>
2018-10-11 02:20:36 -0300

    Alterações

--- server.js
+++ server.js
@@ -5,18 +5,39 @@ let express = require('express');
 let path = require('path');
 let axios = require('axios');
 
+//var cors = require('cors');
+
+// use it before all route definitions
+//app.use(cors({ origin: 'http://localhost:3000' }));
+
 let app = express();
+
+// Add headers
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 let port = process.env.port || 3000
 
 const API = require('./API');
 
 app.use(express.static(__dirname + '/web'));
 app.use(require('body-parser').json());
-
-var cors = require('cors');
-
-// use it before all route definitions
-app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
